726039a0760c0e691f701fa2aadd58fe330c1e8d
edenerez <tempeden@walla.com>
2016-06-30 16:59:56 +0300

    Update the API url to the new Amazon web server url

--- dTiberias/Controllers/JTextMinerServices.js
+++ dTiberias/Controllers/JTextMinerServices.js
@@ -31,7 +31,7 @@ jTextMinerApp.service('fileUpload', ['$http', 'InProgressService', '$location', 
 
 
 jTextMinerApp.factory("APIService", function ($resource) {
-    var url = "http://ec2-52-58-29-166.eu-central-1.compute.amazonaws.com:80/WebServiceJTextMinerNewRoot4/api/JTextMinerAPI";
+    var url = "http://ec2-52-58-251-124.eu-central-1.compute.amazonaws.com:80/WebServiceJTextMinerNewRoot4/api/JTextMinerAPI";
     //url = "http://localhost:8080/NewWebSite/api/JTextMinerAPI";
 
     return $resource(url + "/:crud/:secondParam",
@@ -58,7 +58,7 @@ jTextMinerApp.factory("CAPIService", function ($resource) {
 jTextMinerApp.factory('ExperimentService', function ($rootScope, ClassificationService, SegmentationService, FeatureService, APIService, $location, InProgressService, ClassService, SelectClassService) {
     var service = {};
 
-    service.baseUrl = "http://ec2-52-58-29-166.eu-central-1.compute.amazonaws.com:80/WebServiceJTextMinerNewRoot4/api/JTextMinerAPI";
+    service.baseUrl = "http://ec2-52-58-251-124.eu-central-1.compute.amazonaws.com:80/WebServiceJTextMinerNewRoot4/api/JTextMinerAPI";
     //service.baseUrl = "http://localhost:8080/NewWebSite/api/JTextMinerAPI";
 
     service.user = 'user';
